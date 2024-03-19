5796127b1a922abbf4224e98b6efb927df370ceb
SannyN <50317133+SannyN@users.noreply.github.com>
2024-03-19 20:00:44 +0100

    debug

--- app.py
+++ app.py
@@ -28,6 +28,35 @@ async def root():
 symbol="BTCUSDT"
 category="linear"
 
+""" 
+{
+    "side": "LONG",
+    "entry": "63816.2",
+    "tp1": "64454.4",
+    "tp2": "65092.5",
+    "tp3": "65730.7",
+    "tp4": "66368.8",
+    "winrate": "52%",
+    "strategy": "MANUAL",
+    "beTargetTrigger": "1",
+    "stop": "62810.8"
+}
+
+
+{
+    "side": "SHORT",
+    "entry": "67023.9",
+    "tp1": "66353.7",
+    "tp2": "65683.4",
+    "tp3": "65013.2",
+    "tp4": "64342.9",
+    "winrate": "51.03%",
+    "strategy": "MANUAL",
+    "beTargetTrigger": "1",
+    "stop": "67864.7"
+}
+"""
+
 # Changing mode and leverage: 
 def set_mode():
     try:
@@ -48,10 +77,12 @@ async def webhook(data: str = Body(), secret: str = Query(None)):
     if os.environ["client_secret"] != secret: return {"nice"}
 
     positions = session.get_positions(category=category, symbol=symbol)
+    print("positions")
+    print(positions)
 
     if len(positions["result"]["list"]) > 0:
-        print(positions["result"]["list"])
         print("Cancel all active orders & positions")
+        print(positions["result"]["list"])
         Helpers(session).close_position(category=category, symbol=symbol)
 
     print("webhook")
