85728f0a024b8ed42c1798f655135489863b599f
SannyN <sanny_nguyen@gmx.de>
2024-04-08 11:10:56 +0200

    test

--- app.py
+++ app.py
@@ -81,8 +81,16 @@ async def webhook(data: WebhookData, secret: str = Query(None)):
     dentry = decimal.Decimal(data.entry)
     dwinrate = decimal.Decimal(data.winrate)
     dstop = decimal.Decimal(data.stop)
-    distance = (dentry * 100 / dstop if data.side == "LONG" else dstop * 100 / dentry) - 100
-    trailing = dentry - dstop if data.side == "LONG" else dstop - dentry
+    distance = (dentry * 100 / dstop if data.side == "LONG" else dstop * 100 / dentry) - 10
+
+    if data.side == "LONG":
+        trailingSL = dentry - dstop
+        trailingTP1 = tp1 - entry
+        trailing = trailingSL if trailingSL > trailingTP1 else trailingTP1
+    else:
+        trailingSL = dstop - dentry
+        trailingTP1 = entry - tp1
+        trailing = trailingSL if trailingSL > trailingTP1 else trailingTP1
 
     if dwinrate < 50:
         print("Winrate low")
