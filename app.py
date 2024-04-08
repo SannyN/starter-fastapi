f5a227ffeebb06e6110ab2ba9698fc9275d9dd62
SannyN <sanny_nguyen@gmx.de>
2024-04-08 11:12:14 +0200

    test

--- app.py
+++ app.py
@@ -44,123 +44,123 @@ app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 @app.post("/webhook")
 async def webhook(data: WebhookData, secret: str = Query(None)):
-    getcontext().prec = 3
-    if os.environ["client_secret"] != secret:
-        print("secret")
-        return {"message": "nice"}
-
-    print(data)
-    # BTCUSDT.P
-    symbol = data.ticker.replace(".P", "")
-    # Get account balance
-    
-    walletBalance = session.get_wallet_balance(
-                        accountType="UNIFIED",
-                        coin="USDT",
-                    )
-    print(walletBalance)
-    if walletBalance["retMsg"] == "OK":
-        balance = decimal.Decimal(walletBalance["result"]["list"][0]["totalEquity"])
-    else:
-        print("Failed to get account info")
-        return {"message": "Failed to get account info"}
-    
-    print("Balance")
-    print(balance)
-
-    actual_leverage = data.leverage
-    dactual_leverage = decimal.Decimal(actual_leverage)
-
-    print("actual_leverage")
-    print(actual_leverage)
-
-    dentry = decimal.Decimal(data.entry)
-    dwinrate = decimal.Decimal(data.winrate)
-    dtp1 = decimal.Decimal(data.tp1.value)
-    dtp2 = decimal.Decimal(data.tp2.value)
-
-    dmin_winrate = decimal.Decimal(data.min_winrate)
-    dmin_order = decimal.Decimal(data.min_order)
-
-    dstop = decimal.Decimal(data.stop)
-    distance = (dentry * 100 / dstop if data.side == "LONG" else dstop * 100 / dentry) - 100
-    risk = decimal.Decimal(data.risk) / 100
-    order_distance = dentry - dstop if data.side == "LONG" else dstop - dentry
-
-    dorder_qty = (balance * risk) / order_distance
-
-    if dorder_qty < dmin_order:
-        dorder_qty = dmin_order
-        
-    print("dorder_qty")
-    print(dorder_qty)
-
-    if data.side == "LONG":
-        trailingSL = dentry - dstop
-        trailingTP2 = dtp2 - dentry
-        trailing = trailingSL if trailingSL > trailingTP2 else trailingTP2
-    else:
-        trailingSL = dstop - dentry
-        trailingTP2 = dentry - dtp2
-        trailing = trailingSL if trailingSL > trailingTP2 else trailingTP2
-
-    if dwinrate < dmin_winrate:
-        print("Winrate low")
-        return {"message": "Winrate too low"}
-
-    if distance > 3:
-        print("Distance too high")
-        return {"message": "Distance too high"}
-
-    print(balance)
-
-    print("Cancel all active orders & positions")
-    Helpers(session).close_position(category=category, symbol=symbol)
-
     try:
-        resp = session.set_leverage(
-            category=categoy,
-            symbol=symbol,
-            buyLeverage=str(actual_leverage),
-            sellLeverage=str(actual_leverage),
-        )
-        print(resp)
-    except RuntimeError as error:
-        print(error)
-        print("Failed - continue")
-    except:
-        print("Failed - continue")
+        getcontext().prec = 3
+        if os.environ["client_secret"] != secret:
+            print("secret")
+            return {"message": "nice"}
 
-    try:
-        # Place market order
-        resp = session.place_order(
-            category='linear',
-            symbol=symbol,
-            side='Buy' if data.side == "LONG" else 'Sell',
-            orderType='Market',
-            qty=round(dorder_qty, int(data.precision)),
-            stopLoss=data.stop,
-            slTriggerBy='MarkPrice',
-            positionIdx=0
-        )
-        print(resp)
-    except RuntimeError as error:
-        print(error)
-        print("Failed - continue")
-    except:
-        print("Failed - continue")
-
-    # Place limit orders
-    orders = [data.tp1, data.tp2, data.tp3, data.tp4]
-
-    for order in orders:
-        qty_factor = decimal.Decimal(order.percentage) / 100
-        price = decimal.Decimal(order.value)
-
-        if qty_factor == 0:
-            continue
+        print(data)
+        # BTCUSDT.P
+        symbol = data.ticker.replace(".P", "")
+        # Get account balance
         
+        walletBalance = session.get_wallet_balance(
+                            accountType="UNIFIED",
+                            coin="USDT",
+                        )
+        print(walletBalance)
+        if walletBalance["retMsg"] == "OK":
+            balance = decimal.Decimal(walletBalance["result"]["list"][0]["totalEquity"])
+        else:
+            print("Failed to get account info")
+            return {"message": "Failed to get account info"}
+        
+        print("Balance")
+        print(balance)
+
+        actual_leverage = data.leverage
+        dactual_leverage = decimal.Decimal(actual_leverage)
+
+        print("actual_leverage")
+        print(actual_leverage)
+
+        dentry = decimal.Decimal(data.entry)
+        dwinrate = decimal.Decimal(data.winrate)
+        dtp1 = decimal.Decimal(data.tp1.value)
+        dtp2 = decimal.Decimal(data.tp2.value)
+
+        dmin_winrate = decimal.Decimal(data.min_winrate)
+        dmin_order = decimal.Decimal(data.min_order)
+
+        dstop = decimal.Decimal(data.stop)
+        distance = (dentry * 100 / dstop if data.side == "LONG" else dstop * 100 / dentry) - 100
+        risk = decimal.Decimal(data.risk) / 100
+        order_distance = dentry - dstop if data.side == "LONG" else dstop - dentry
+
+        dorder_qty = (balance * risk) / order_distance
+
+        if dorder_qty < dmin_order:
+            dorder_qty = dmin_order
+            
+        print("dorder_qty")
+        print(dorder_qty)
+
+        if data.side == "LONG":
+            trailingSL = dentry - dstop
+            trailingTP2 = dtp2 - dentry
+            trailing = trailingSL if trailingSL > trailingTP2 else trailingTP2
+        else:
+            trailingSL = dstop - dentry
+            trailingTP2 = dentry - dtp2
+            trailing = trailingSL if trailingSL > trailingTP2 else trailingTP2
+
+        if dwinrate < dmin_winrate:
+            print("Winrate low")
+            return {"message": "Winrate too low"}
+
+        if distance > 3:
+            print("Distance too high")
+            return {"message": "Distance too high"}
+
+        print(balance)
+
+        print("Cancel all active orders & positions")
+        Helpers(session).close_position(category=category, symbol=symbol)
+
         try:
+            resp = session.set_leverage(
+                category=categoy,
+                symbol=symbol,
+                buyLeverage=str(actual_leverage),
+                sellLeverage=str(actual_leverage),
+            )
+            print(resp)
+        except RuntimeError as error:
+            print(error)
+            print("Failed - continue")
+        except:
+            print("Failed - continue")
+
+        try:
+            # Place market order
+            resp = session.place_order(
+                category='linear',
+                symbol=symbol,
+                side='Buy' if data.side == "LONG" else 'Sell',
+                orderType='Market',
+                qty=round(dorder_qty, int(data.precision)),
+                stopLoss=data.stop,
+                slTriggerBy='MarkPrice',
+                positionIdx=0
+            )
+            print(resp)
+        except RuntimeError as error:
+            print(error)
+            print("Failed - continue")
+        except:
+            print("Failed - continue")
+
+        # Place limit orders
+        orders = [data.tp1, data.tp2, data.tp3, data.tp4]
+
+        for order in orders:
+            qty_factor = decimal.Decimal(order.percentage) / 100
+            price = decimal.Decimal(order.value)
+
+            if qty_factor == 0:
+                continue
+            
             resp = session.place_order(
                 category='linear',
                 symbol=symbol,
@@ -173,15 +173,7 @@ async def webhook(data: WebhookData, secret: str = Query(None)):
                 reduceOnly=True
             )
             print(resp)
-        except RuntimeError as error:
-            print(error)
-            print("Failed - continue")
-        except:
-            print("Failed - continue")
-
-    try:
-        # Set trading stop
-        # Config this
+            
         resp = session.set_trading_stop(
             category=category,
             symbol=symbol,
@@ -189,12 +181,10 @@ async def webhook(data: WebhookData, secret: str = Query(None)):
             activePrice=data.tp2.value,
             positionIdx=0
         )
-        print(resp)
+            print(resp)
     except RuntimeError as error:
         print(error)
         print("Failed - continue")
     except:
         print("Failed - continue")
-
-    print(data)
     return {"nice"}
