01fb9297a6e5f61a6084742a8e5256946ebd4a3f
SannyN <sanny_nguyen@gmx.de>
2024-04-08 11:12:34 +0200

    test

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ async def webhook(data: WebhookData, secret: str = Query(None)):
             symbol=symbol,
             side='Buy' if data.side == "SHORT" else 'Sell',
             orderType='Limit',
-            qty=round(float(dorder_qty) * qty_factor, int(data.precision)),
+            qty=round(float(dorder_qty*qty_factor), int(data.precision)),
             timeInForce="PostOnly",
             positionIdx=0,
             price=price,
